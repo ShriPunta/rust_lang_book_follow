3d7b4e4dcac5151ac08ae6a0343db9105a898516
Shridhar <shridhar.puntambekar@gmail.com>
2022-11-06 18:18:10 -0500

    Fibo exercise

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,22 @@
-
+use std::io;
 fn main() {
-    let x: (i32,f64,u8,char) = (500,6.3,3,'s');
-    println!("{:?}",x);
-    let five_hundo=x.0;
-    let s_char=x.3;
-    println!("{five_hundo} {s_char}");
+    let mut range_fibo = String::new();
+    println!("Please give number of digits expected ");
+    io::stdin()
+    .read_line(&mut range_fibo)
+    .expect("Failed to read line");
+    let range_fibo: u32 = range_fibo.trim().parse().expect("PANIKK!");
+    println!("{}", fibo_gen(range_fibo));
+}
 
-    const A: [i32; 5] = [1,2,3,4,5];
-    println!("{:#?}", A);
-
-    let t = {
-        let x = 3;
-        x+1
-    };
-    println!("value of T {t}");
-    let mut count = 0;
-    let result = loop {
-        count += 1;
-        if count == 10{
-            break count*5;
+fn fibo_gen(x:u32)->u32 {
+    match x {
+        0 => return 1,
+        1 => return 1,
+        _ => {
+            let s = fibo_gen(x-1)+ fibo_gen(x-2);
+            print!("{s}..");
+            return s;
         }
-        println!("Count now {count}");
-    };
-    println!("Count Res {result}");
-
-    let mut counta = 0;
-    'counting_up: loop {
-        println!("counta = {counta}");
-        let mut remaining = 10;
-
-        loop {
-            println!("remaining = {remaining}");
-            if remaining == 9 {
-                break;
-            }
-            if counta == 2 {
-                break 'counting_up;
-            }
-            remaining -= 1;
-        }
-
-        counta += 1;
-
-        
-    }
-    println!("End count = {counta}");
-    for number in (1..4).rev() {
-        println!("{number}!");
     }
 }
