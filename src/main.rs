1ff68ddb4b939d9c7134d1b720f36b7ba63e0728
Shridhar Puntambekar <shridhar.puntambekar@gmail.com>
2022-11-21 18:43:08 -0500

    Median mode vector exercise

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use std::collections::HashMap;
+//use rand::{thread_rng, Rng};
 
 // Standard library enum: Option
 fn main() {
+    let ints: Vec<u8> = vec![2, 9, 2, 5, 6, 2, 4, 5, 8, 2, 4, 5, 6, 3, 2, 8, 2, 2, 4];
+    //thread_rng().fill(&mut ints[..]);
+    getmedian_mode(&ints);
+    /*
     let mut v= vec![120,240];
     for i in &mut v{
         println!("{}",*i+100);
-    }    
+    }
     let mut s1 = "LoL".to_string();
     s1.push_str("zzzz");
     let mut s2 = String::from("d");
@@ -18,7 +23,7 @@ fn main() {
     let s6 = String::from("toe");
     let s7 = format!("{}-{}-{}",s4,s5,s6);
     println!("{}",s7);
-    
+
     let mut scores = HashMap::new();
     scores.insert(String::from("blue"), 200);
     scores.insert(String::from("red"), 500);
@@ -44,6 +49,33 @@ fn main() {
     }
     println!("{:?}", worldMap);
 
-
+    */
 }
 
+fn getmedian_mode(arr: &[u8]) {
+    let mut ele_by_cnt_map = HashMap::new();
+    let mut mode_val: u8 = 0;
+    let mut sorted_arr = arr.to_vec();
+    sorted_arr.sort();
+    for ele in arr {
+        // First time element is encountered
+        let cnt = ele_by_cnt_map.entry(ele).or_insert(0);
+        *cnt += 1;
+        mode_val = if cnt >= &mut mode_val { *cnt } else { mode_val };
+    }
+    println!("{:#?}", ele_by_cnt_map);
+    println!("Modal {:?}", mode_val);
+    println!(" Sorted Array {:?}", sorted_arr);
+    let median:f64 = {
+        let midpt = sorted_arr.len() % 2;
+        let mut med:f64 = 0.0;
+        if midpt%2==0{
+            med=sorted_arr[midpt].into();
+        }else{
+            med=((sorted_arr[midpt+1]+sorted_arr[midpt])/2).into();
+        }
+        med
+    };
+    println!(" Median {:?}", median);
+    println!(" Array size {:?}", sorted_arr.len());
+}
